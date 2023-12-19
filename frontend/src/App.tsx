0ae595d49a2dc5ea853ec118f6094ebe9d5a8a90
Justinding765 <justin.ding@mail.utoronto.ca>
2023-12-19 17:10:39 -0500

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/hompage';
+import Translation from './pages/Translation';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/translation" element={<Translation />} />
         {/* Add other routes here */}
       </Routes>
     </Router>
--- frontend/src/pages/hompage.tsx
+++ frontend/src/pages/hompage.tsx
@@ -1,33 +1,39 @@
-// src/pages/HomePage.tsx
+import { useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
-import '../App.css';
-
+import '../App.css'
 const HomePage: React.FC = () => {
+    const navigate = useNavigate();
     const [url, setUrl] = useState('');
+    const [loading, setLoading] = useState(false);  // New state for loading
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        setLoading(true);  // Set loading to true when the request starts
 
         try {
-            const response = await fetch('http://localhost:5000/web_scrapper', {
+            const response = await fetch('http://localhost:5000/translate_images', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ url: url }), // Sending the URL as JSON
+                body: JSON.stringify({ url: url }),
             });
+
 
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
-
+            setLoading(false);  // Set loading to false when the request completes
             const data = await response.json();
-            console.log('Response from server:', data);
-            // Handle the response data
+            
+            // Redirect and pass the image URLs
+            navigate('/translation', { state: { imageUrls: data } });
         } catch (error) {
             console.error('Error submitting URL:', error);
+            setLoading(false);
         }
     };
+
 
     return (
         <div>
@@ -45,7 +51,9 @@ const HomePage: React.FC = () => {
                         className="homepage-input"
                         placeholder="https://comic.naver.com/webtoon/detail?titleId=641253&no=472&week=fri"
                     />
-                    <button type="submit" className="homepage-button">Submit</button>
+                     <button type="submit" className="homepage-button" disabled={loading}>
+                        {loading ? <div className="loading-spinner"></div> : "Submit"}
+                    </button>
                 </form>
             </div>
         </div>
