271454f14800afae464b91909937673a6dbc0b6f
Justinding765 <justin.ding@mail.utoronto.ca>
2024-01-07 10:35:34 -0500

    test

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,14 +1,14 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function(app) {
-//   app.use(
-//     '/api',
-//     createProxyMiddleware({
-//       target: 'http://backend:5000',
-//       changeOrigin: true,
-//       pathRewrite: {'^/api': ''},
-//       timeout: 200000, // Timeout in milliseconds
-//       proxyTimeout: 200000, // Timeout in milliseconds
-//     })
-//   );
-// };
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://backend:5000',
+      changeOrigin: true,
+      pathRewrite: {'^/api': ''},
+      timeout: 200000, // Timeout in milliseconds
+      proxyTimeout: 200000, // Timeout in milliseconds
+    })
+  );
+};
